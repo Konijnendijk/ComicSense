265a01c868513cea5c3a0e0de32248c6515e654c
Max Oomen <130802@nhtv.nl>
2015-01-24 00:47:22 +0100

    merged playerMovement and playerAttack, player can now kill the flies

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+  enum State
+  {
+    IDLE,
+    ATTACKING,
+    SELECTING_TRAP,
+  }
+
+  public float m_movementSpeed;
+  public float m_attackDelay;
+  public float m_swingTime;
+  public GameObject m_attackCollider;
+  float m_lastAttackTime;
+
+  GameObject m_colliderToBeCleaned;
+
+  bool IsAttacking()
+  {
+    return m_lastAttackTime + m_swingTime >= Time.time;
+  }
+
+  void Attack()
+  {
+    GameObject go = (GameObject)Instantiate(m_attackCollider);
+    go.transform.parent = transform;
+    go.transform.localPosition = new Vector3(0, 0, 1.235f);
+    go.transform.localRotation = Quaternion.identity;
+    m_colliderToBeCleaned = go;
+  }
+
+  void Update()
+  {
+    if(m_colliderToBeCleaned != null && !IsAttacking())
+    {
+      Destroy(m_colliderToBeCleaned);
+    }
+
+    if(Input.GetButtonDown("Attack") &&
+      m_lastAttackTime + m_attackDelay < Time.time)
+    {
+      m_lastAttackTime = Time.time;
+      Attack();
+    }
+  }
+
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    Vector3 axisLeft = new Vector3(
+        Input.GetAxis("leftStickX"),
+        0.0f,
+        -Input.GetAxis("leftStickY"));
+
+    if(axisLeft != Vector3.zero && !IsAttacking())
+    {
+      transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        Quaternion.LookRotation(axisLeft, Vector3.up),
+        Time.fixedDeltaTime * 5.0f
+        );
+
+      //transform.forward = axisLeft;
+      transform.Translate(Vector3.forward * axisLeft.magnitude * m_movementSpeed * Time.fixedDeltaTime);
+    }
+  }
+}
---
+++ UnityProject/Assets/Scripts/PlayerSword.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSword : MonoBehaviour
+{
+  void OnTriggerEnter(Collider collider)
+  {
+    if(collider.tag == "Flies")
+      Destroy(collider.gameObject);
+  }
+
+}
