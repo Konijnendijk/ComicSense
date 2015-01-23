d1039c3dac285d48128a9fb1ddc13c6e8959041f
Max Oomen <130802@nhtv.nl>
2015-01-23 18:47:53 +0100

    added untiy VS tools, and globals scripts.\n

---
+++ UnityProject/Assets/Scripts/Globals.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour
+{
+
+  //getters and setters
+
+  //variables
+
+  //public variables for the debugging
+
+
+  //PLEASE DO NOT TOUCH
+  static Globals s_instance = null;
+  public static Globals GetInstance()
+  {
+    if(s_instance)
+      return s_instance;
+
+    GameObjec go = new GameObject();
+    go.name = "Globals";
+
+    s_instance = go.AddComponent<Globals>();
+
+    return s_instance;
+  }
+  public static Globals GI() { return GetInstance(); }
+}
