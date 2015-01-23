e33fbfeebad25eca706aab522a7e67641d2f94b4
Max Oomen <130802@nhtv.nl>
2015-01-23 22:30:39 +0100

    player movement and camera movement

---
+++ UnityProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+  public GameObject m_player;
+  public Vector3 m_cameraPosOffset;
+  public float m_deadZone;
+
+  void Update()
+  {
+    Vector3 targetPos = m_player.transform.position + m_cameraPosOffset;
+    if((transform.position - targetPos).sqrMagnitude < m_deadZone * m_deadZone)
+      transform.position = targetPos;
+    else
+    {
+      transform.position = Vector3.Lerp(
+        transform.position,
+        m_player.transform.position + m_cameraPosOffset,
+        Time.deltaTime * 5.0f
+        );
+    }
+  }
+}
---
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float m_movementSpeed;
+
+
+  void FixedUpdate()
+  {
+    Vector3 axisLeft = new Vector3(
+      Input.GetAxis("leftStickX"),
+      0.0f,
+      -Input.GetAxis("leftStickY"));
+
+    if(axisLeft != Vector3.zero)
+    {
+      transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        Quaternion.LookRotation(axisLeft, Vector3.up),
+        Time.fixedDeltaTime * 5.0f
+        );
+
+      //transform.forward = axisLeft;
+      transform.Translate(Vector3.forward * m_movementSpeed * Time.fixedDeltaTime);
+    }
+  }
+}
