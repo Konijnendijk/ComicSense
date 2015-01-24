b7842d1a331da93f89adbf5977e9bed92a5f1fad
Geert Konijnendijk <geert@konijnendijk.info>
2015-01-24 20:15:02 +0100

    Added scream text bubble.

--- UnityProject/Assets/Scripts/ScreamManager.cs
+++ UnityProject/Assets/Scripts/ScreamManager.cs
@@ -5,9 +5,6 @@ using UnityEngine.UI;
 public class ScreamManager : MonoBehaviour {
 
     public Image ScreamMeter;
-    public Image North, East, South, West;
-
-    public Transform Knight;
 
     //The amount of scream being subtracted every second
     public float RecoveryPerSecond;
@@ -21,11 +18,33 @@ public class ScreamManager : MonoBehaviour {
     // 0 is no screams, 1 is game over
     private float m_scream;
 
+    /*public void Start()
+    {
+        North.enabled = false;
+        East.enabled = false;
+        South.enabled = false;
+        West.enabled = false;
+    }*/
+
+
+
     /// <summary>
     /// Princess screams, play sound and add to scream meter
     /// </summary>
     public void Scream (Room room, int numFlies){
-        m_scream += ScreamIncrement * numFlies;       
+        m_scream += ScreamIncrement * numFlies;
+    }
+
+    IEnumerator FadeOut(GameObject img)
+    {
+        Material mat = img.GetComponent<Image>().material;
+        float newAlpha=mat.color.a- 5f * Time.deltaTime;
+        mat.color = new Color(mat.color.r,mat.color.g,mat.color.b, newAlpha<0?0:newAlpha );
+
+        if (mat.color.a <= 0)
+            Destroy( img.gameObject);
+        else
+            yield return null;
     }
 
     public void Update()
