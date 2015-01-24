98671dd8c5016a1d781b12b44de91c50811d1917
Geert Konijnendijk <geert@konijnendijk.info>
2015-01-24 09:56:10 +0100

    Added logic for triggering a scream and displaying the amount of scream on-screen.

---
+++ UnityProject/Assets/Scripts/Room.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Globals.GetInstance().FliesTag)
+        {
+            GameObject.FindObjectOfType<ScreamManager>().Scream(this);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/ScreamManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreamManager : MonoBehaviour {
+
+    public Image ScreamMeter;
+
+    //The amount of scream being subtracted every second
+    public float RecoveryPerSecond;
+
+    //Amount of scream to be added when a princess screams
+    public float ScreamIncrement;
+
+
+    // 0 is no screams, 1 is game over
+    private float m_scream;
+
+    /// <summary>
+    /// Princess screams, play sound and add to scream meter
+    /// </summary>
+    public void Scream (Room room){
+        m_scream += ScreamIncrement;
+    }
+
+    public void Update()
+    {
+        m_scream -= m_scream<0?0:  RecoveryPerSecond * Time.deltaTime;
+        Image meter=GameObject.FindGameObjectWithTag(Globals.GetInstance().ScreamMeterTag).GetComponent<Image>();
+        meter.fillAmount = m_scream;
+    }
+	
+}
