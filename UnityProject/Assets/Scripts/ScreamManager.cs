8b68857831262da4b73ad4cca3453ebab9a483f7
Geert Konijnendijk <geert@konijnendijk.info>
2015-01-25 13:48:39 +0100

    Updated main menu.

--- UnityProject/Assets/Scripts/ScreamManager.cs
+++ UnityProject/Assets/Scripts/ScreamManager.cs
@@ -14,12 +14,18 @@ public class ScreamManager : MonoBehaviour {
 
     // 0 is no screams, 1 is game over
     private float m_scream;
+    private float m_gameOverTimer = 10;
 
     /// <summary>
     /// Princess screams, play sound and add to scream meter
     /// </summary>
     public void Scream (){
         m_scream += ScreamIncrement;
+
+        if (m_scream >= 0)
+        {
+            gameOver();
+        }
     }
 
     public void Update()
@@ -27,5 +33,33 @@ public class ScreamManager : MonoBehaviour {
         m_scream -= m_scream<0?0:  RecoveryPerSecond * Time.deltaTime;
         ScreamMeter.fillAmount = m_scream;
     }
-	
+
+    private void gameOver()
+    {
+        GameObject.Find("Flyspawner").SetActive(false);
+        GameObject.Find("Player").SetActive(false);
+
+        StartCoroutine("GameOver");
+    }
+
+
+    IEnumerator GameOver()
+    {
+        while (m_gameOverTimer > 0)
+        {
+            m_gameOverTimer -= Time.deltaTime;
+
+            Text text = GameObject.Find("GameOver").GetComponent<Text>();
+
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + 1 * Time.deltaTime);
+
+
+            if (m_gameOverTimer <= 0)
+                Application.LoadLevel("mainMenu");
+            else
+                yield return null;
+        }
+
+        
+    }
 }
